45f5a2054f778d2e3b3456a30e1e50b816e8aec4
Harsimran singh <singhharsimran5200@gmail.com>
2026-03-07 21:03:26 +0530

    Smooth Scrolling (Locomotive JS) Done

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,27 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
+import { useEffect, useRef } from "react";
+import LocomotiveScroll from "locomotive-scroll";
+import "locomotive-scroll/dist/locomotive-scroll.css";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: scrollRef.current as HTMLElement,
+      smooth: true,
+    });
+
+    return () => {
+      scroll.destroy();
+    };
+  }, []);
+
+  return (
+    <div data-scroll-container ref={scrollRef}>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
